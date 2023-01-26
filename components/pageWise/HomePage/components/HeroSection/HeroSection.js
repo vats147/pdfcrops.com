b6dfc5e16f555c62a351d42e064d3c67e9ee57a5
Anurag <anuragmarketing101@gmail.com>
2023-01-26 15:43:54 +0530

    heroSection-bg-changed

--- components/pageWise/HomePage/components/HeroSection/HeroSection.js
+++ components/pageWise/HomePage/components/HeroSection/HeroSection.js
@@ -3,19 +3,19 @@ import React from 'react'
 import heroImage from "../../../../../public/images/heroImage.png"
 
 
-// h-[45vh] lg:h-[70vh] xl:h-[65vh]
+// bg-gradient-to-b from-sky-500 via-fuchsia-400 to-gray-100
 
 const HeroSection = () => {
   return (
-    <section className='w-full border border-black pt-14 md:pt-20 flex flex-col justify-start items-center lg:flex-row lg:items-center lg:justify-center lg:bg-gradient-to-b from-sky-500 via-fuchsia-400 to-gray-100'> 
+    <section className='w-full py-10 md:py-20 flex flex-col justify-start items-center bg-gradient-to-b from-sky-500 via-fuchsia-400 to-gray-100 lg:flex-row lg:items-center lg:justify-center lg:bg-black'> 
 
       {/* ---- Text Section ---- */}
-      <div className='w-full h-full lg:w-[50%] flex flex-col justify-start lg:justify-center items-center pt-20 bg-gradient-to-b from-sky-500 via-fuchsia-400 to-gray-100 lg:bg-transparent'>
+      <div className='w-full h-full lg:w-[50%] flex flex-col justify-start lg:justify-center items-center pt-20 '>
         <div className='w-[90%] sm:w-[80%] md:w-[60%] lg:w-[80%] xl:w-[60%] lg:text-left flex flex-col items-center justify-start text-center space-y-2 lg:space-y-3'>
 
           <h3 className='text-[#1A4971] text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-bold '> Your Productivity Lives at Xodo </h3>
-          <p className='text-[#343A40] text-base sm:text-xl md:text-2xl font-medium '> Try easy-to-use PDF tools that keep your documents safe </p>
-          <p className='text-[#343A40] text-sm sm:text-base font-normal '> Securely view, annotate, and work with your important files across all platforms and devices with the ultimate all-in-one document solution. </p>
+          <p className='text-[#343A40] text-base sm:text-xl lg:text-2xl font-medium '> Try easy-to-use PDF tools that keep your documents safe </p>
+     
 
         </div>
       </div>
@@ -30,7 +30,7 @@ const HeroSection = () => {
           alt="hero-image"
           width={30}
           height={30}
-          className="w-full lg:w-[45%] xl:w-[45%] 2xl:w-[35%] aspect-square " />
+          className="w-full lg:w-[40%] xl:w-[40%] 2xl:w-[30%] aspect-square" />
 
       </div>
 
