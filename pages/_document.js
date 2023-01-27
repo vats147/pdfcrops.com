be1d191e3157cd2a479e2fe2397df08eaa4774c5
Vatsal Mangukiya <89577062+vats147@users.noreply.github.com>
2023-01-27 21:25:48 +0530

    Remove Adsenese

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-    <Head> <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5174630229786506" crossorigin="anonymous"></script>
+    <Head> 
       <Head /> 
       <body>
         <Main />
