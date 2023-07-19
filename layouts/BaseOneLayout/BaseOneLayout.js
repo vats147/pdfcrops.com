366d5f28e691b7748ae8ed8d89f9ed5f01f5ec53
Vatsal Mangukiya <89577062+vats147@users.noreply.github.com>
2023-07-19 12:50:52 +0530

    update head
     After:-> PDF Crops | Free Online Label Crop | GlowRoad Meesho FlipCart

--- layouts/BaseOneLayout/BaseOneLayout.js
+++ layouts/BaseOneLayout/BaseOneLayout.js
@@ -19,16 +19,16 @@ const BaseOneLayout = ({ children }) => {
     return (
         <div className='w-full h-auto bg-gray-100 flex flex-col justify-start items-center overflow-x-hidden overflow-y-scroll'>
             <Head>
-                <title> PDF Crops | Your Cloud PDF Tools & Online Label Crop</title>
+                <title> PDF Crops | Free Online Label Crop | GlowRoad Meesho Flipkart  </title>
                 <link rel="canonical" href="https://pdfcrops.com/"></link>
 
-                <meta property="og:description" content="pdfcrops is best and safe site for tool such as crop and process Amazon, Flipkart and other e-commerce label, merge crop pdf"></meta>
+                <meta property="og:description" content="pdfcrops is best, free and safe site for tool such as crop and process Amazon, Flipkart and other e-commerce label, merge crop pdf"></meta>
 
                 <meta property="og:url" content="https://pdfcrops.com/"></meta>
 
-                <meta property="og:title" content="pdfcrops | Your Auto Crop PDF tools"></meta>
+                <meta property="og:title" content=" PDF Crops | Free Online Label Crop | GlowRoad Meesho Flipkart "></meta>
 
-                <meta name="keywords" content="pdfcrops, pdf crops,crop label online,best online PDF tools, Amazon label pdf crop,Flipkart label pdf crop, Meesho label pdf crop,Merge PDF,Free online label crop, split PDF, combine PDF, extract PDF, compress PDF, convert PDF, Word to PDF, Excel to PDF, Powerpoint to PDF, PDF to JPG, JPG to PDF"></meta>
+                <meta name="keywords" content="glowroad label crop, meesho label crop, pdf crop meesho,meesho label crop online, free label crop pdf crops,crop label online,best online PDF tools, Amazon label pdf crop,Flipkart label pdf crop, Meesho label pdf crop,Merge PDF,Free online label crop,glowroad label crop, meesho label crop "></meta>
 
                 <meta property="og:description" content="PDF crops is a set of free, simple online tools for PDF file editing. automatically crop any Ecommerce PDF"></meta>
 
